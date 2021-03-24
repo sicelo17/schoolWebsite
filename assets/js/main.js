5d09f46fa0ea562e638745cef80ec237547d3cdf
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-03-24 11:21:07 +0200

    nav js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,56 @@ navClose.addEventListener("click", () => {
     navContainer.style.left = "-30rem";
     navContainer.style.width = "0";
 });
+
+
+/*
+=============
+Fixed Navigation
+=============
+ */
+
+const navBar = document.querySelector(".navigation");
+const gotoTop = document.querySelector(".goto-top");
+
+// Smooth Scroll
+Array.from(scrollLink).map(link => {
+    link.addEventListener("click", e => {
+        // Prevent Default
+        e.preventDefault();
+
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        const navHeight = navBar.getBoundingClientRect().height;
+        const fixNav = navBar.classList.contains("fix__nav");
+        let position = element.offsetTop - navHeight;
+
+        if (!fixNav) {
+            position = position - navHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        navContainer.style.left = "-30rem";
+        document.body.classList.remove("active");
+    });
+});
+
+// Fix NavBar
+
+window.addEventListener("scroll", e => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navBar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navBar.classList.add("fix__nav");
+    } else {
+        navBar.classList.remove("fix__nav");
+    }
+
+    if (scrollHeight > 300) {
+        gotoTop.classList.add("show-top");
+    } else {
+        gotoTop.classList.remove("show-top");
+    }
+});
