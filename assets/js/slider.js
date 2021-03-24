7c71a71b1c53cd4eab90c6c5adaf891dcb0a0aae
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-03-24 12:44:26 +0200

    slider js updated

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -26,3 +26,88 @@ if (slider1) {
         animationTimingFunc: "linear",
     }).mount();
 }
+/*
+=============
+Latest Products
+=============
+ */
+
+if (slider2) {
+    new Glide("#glide_2", {
+        type: "carousel",
+        startAt: 0,
+        perView: 4,
+        rewin: false,
+        animationDuration: 800,
+        animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+        breakpoints: {
+            1200: {
+                perView: 3,
+            },
+            768: {
+                perView: 2,
+            },
+        },
+    }).mount();
+}
+
+/*
+=============
+Related Products
+=============
+ */
+
+if (slider3) {
+    new Glide("#glide_3", {
+        type: "carousel",
+        startAt: 0,
+        perView: 4,
+        rewin: false,
+        animationDuration: 800,
+        animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+        breakpoints: {
+            1200: {
+                perView: 3,
+            },
+            768: {
+                perView: 2,
+            },
+        },
+    }).mount();
+}
+
+// Testimonial
+
+if (slider4) {
+    new Glide("#glide_4", {
+        type: "carousel",
+        startAt: 0,
+        perView: 1,
+        rewin: false,
+        animationDuration: 800,
+        animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    }).mount();
+}
+
+// News
+if (slider5) {
+    new Glide("#glide_5", {
+        type: "carousel",
+        startAt: 0,
+        perView: 3,
+        rewin: false,
+        autoplay: 3000,
+        animationDuration: 800,
+        animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+        breakpoints: {
+            998: {
+                perView: 2,
+            },
+            768: {
+                perView: 1,
+            },
+        },
+    }).mount();
+}
+
+AOS.init();
