e07c77b9329761c98136dd88c477aa8c53318fe1
sicelo17 <70627871+sicelo17@users.noreply.github.com>
2021-03-24 13:01:31 +0200

    popup js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,26 @@ navClose.addEventListener("click", () => {
     navContainer.style.width = "0";
 });
 
+/*
+=============
+PopUp
+=============
+ */
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup__close");
+
+if (popup) {
+    closePopup.addEventListener("click", () => {
+        popup.classList.add("hide__popup");
+    });
+
+    window.addEventListener("load", () => {
+        setTimeout(() => {
+            popup.classList.remove("hide__popup");
+        }, 500);
+    });
+}
+
 
 /*
 =============
